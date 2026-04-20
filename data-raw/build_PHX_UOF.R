88259bf714bceb47e9ee586eab1edc7fc05933b4
jacobtnyoung <CRJ605@asu.edu>
2026-04-20 13:32:51 -0700

    revisions to age variable

--- data-raw/build_PHX_UOF.R
+++ data-raw/build_PHX_UOF.R
@@ -1,6 +1,10 @@
 # ------------------------------ #
 # Build PHX USE OF FORCE data file
 
+
+# ----
+# Setup
+
 rm( list = ls() )
 
 library( dplyr )
@@ -14,8 +18,24 @@ url <- paste(
 
 dat <- read.csv(url, as.is = TRUE, header = TRUE)
 
-phx_uof_2018_2024 <- dat |>
-  filter(INC_YEAR != 2025) |>
-  filter(INC_YEAR != 2017)
 
-usethis::use_data(phx_uof_2018_2024, overwrite = TRUE)
+# ----
+# Cleanup
+
+# take valid years
+dat <- dat |>
+  filter( INC_YEAR != 2025 ) |>
+  filter( INC_YEAR != 2017 )
+
+# fix the age variable
+
+dat <- dat |>
+  mutate( CIT_AGE = as.numeric( na_if( CIT_AGE, "Not Applicable" ) ) )
+
+
+# ----
+# Save
+
+phx_uof_2018_2024 <- dat
+
+usethis::use_data( phx_uof_2018_2024, overwrite = TRUE )
